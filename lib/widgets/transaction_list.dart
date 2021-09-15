63c14f779f9e98583092de8cf2bb6032601f8250
aecf <81099969+albertoecf@users.noreply.github.com>
2021-09-15 08:22:40 -0300

    Refactor, extract widgets Transactions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
-import 'package:intl/intl.dart';
+import 'transaction_item.dart';
+
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -35,50 +36,11 @@ class TransactionList extends StatelessWidget {
             })
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 8,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child: FittedBox(
-                          child: Text('\$${transactions[index].amount}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMMd().format(transactions[index].date),
-                    ),
-                    trailing: MediaQuery.of(context).size.width > 300
-                        ? FlatButton.icon(
-                            textColor: Theme.of(context).errorColor,
-                            onPressed: () => deleTx(
-                              transactions[index].id,
-                            ),
-                            icon: Icon(Icons.delete),
-                            label: Text('Eliminar'),
-                          )
-                        : IconButton(
-                            icon: Icon(Icons.delete),
-                            color: Theme.of(context).errorColor,
-                            onPressed: () => deleTx(
-                              transactions[index].id,
-                            ),
-                          ),
-                  ),
-                );
+                return TransactionItem(transaction: transactions[index], deleTx: deleTx);
               },
               itemCount: transactions.length,
             ),
     );
   }
 }
+
